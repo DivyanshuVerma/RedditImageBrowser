8ef91ca70c0065c5b3539e9d0dc2889adc7e1012
Divyanshu Verma <dev.verma1010@gmail.com>
2012-04-07 04:52:45 -0400

    added project

--- UI.java
+++ UI.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 class UI extends JFrame implements ActionListener
 {
 	JButton prev, next;
+	JLabel info;
 	UI()
 	{
 		super("Reddit Image Browser");
@@ -28,8 +29,12 @@ class UI extends JFrame implements ActionListener
 		next.setBounds(700,0,100,30);
 		next.addActionListener(this);
 
+		info = new JLabel("");
+		info.setBounds(100,400,500,30);
+
 		c.add(prev);
 		c.add(next);
+		c.add(info);
 
 		this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -38,6 +43,15 @@ class UI extends JFrame implements ActionListener
 
 	public void actionPerformed(ActionEvent e)
 	{
+		if(e.getSource()==prev)
+		{
+			info.setText("This button will display the previous image.");
+		}
+		else if(e.getSource()==next)
+		{
+			info.setText("This button will display the next image.");
+		}
+		else;
 	}
 
 	public static void main(String abc[])
